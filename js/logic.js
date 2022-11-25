7ce8f9c60dcfc8c098e6acd529ca71dd89215c68
Flak <FlakTheMighty@users.noreply.github.com>
2022-11-25 16:57:37 -0600

    Added Gen 2 Pokemon
    Gen 2 Pokemon have been added for Pokemon Stadium 2 team generation

--- js/logic.js
+++ js/logic.js
@@ -1,13 +1,20 @@
 const baseSpriteURL = 'https://img.pokemondb.net/sprites/ruby-sapphire/normal/'
-const pokemonNames = ['Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise', 'Caterpie', 'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot', 'Rattata', 'Raticate', 'Spearow', 'Fearow', 'Ekans', 'Arbok', 'Pikachu', 'Raichu', 'Sandshrew', 'Sandslash', 'Nidoran♀', 'Nidorina', 'Nidoqueen', 'Nidoran♂‚', 'Nidorino', 'Nidoking', 'Clefairy', 'Clefable', 'Vulpix', 'Ninetales', 'Jigglypuff', 'Wigglytuff', 'Zubat', 'Golbat', 'Oddish', 'Gloom', 'Vileplume', 'Paras', 'Parasect', 'Venonat', 'Venomoth', 'Diglett', 'Dugtrio', 'Meowth', 'Persian', 'Psyduck', 'Golduck', 'Mankey', 'Primeape', 'Growlithe', 'Arcanine', 'Poliwag', 'Poliwhirl', 'Poliwrath', 'Abra', 'Kadabra', 'Alakazam', 'Machop', 'Machoke', 'Machamp', 'Bellsprout', 'Weepinbell', 'Victreebel', 'Tentacool', 'Tentacruel', 'Geodude', 'Graveler', 'Golem', 'Ponyta', 'Rapidash', 'Slowpoke', 'Slowbro', 'Magnemite', 'Magneton', "Farfetch'd", 'Doduo', 'Dodrio', 'Seel', 'Dewgong', 'Grimer', 'Muk', 'Shellder', 'Cloyster', 'Gastly', 'Haunter', 'Gengar', 'Onix', 'Drowzee', 'Hypno', 'Krabby', 'Kingler', 'Voltorb', 'Electrode', 'Exeggcute', 'Exeggutor', 'Cubone', 'Marowak', 'Hitmonlee', 'Hitmonchan', 'Lickitung', 'Koffing', 'Weezing', 'Rhyhorn', 'Rhydon', 'Chansey', 'Tangela', 'Kangaskhan', 'Horsea', 'Seadra', 'Goldeen', 'Seaking', 'Staryu', 'Starmie', 'Mr. Mime', 'Scyther', 'Jynx', 'Electabuzz', 'Magmar', 'Pinsir', 'Tauros', 'Magikarp', 'Gyarados', 'Lapras', 'Ditto', 'Eevee', 'Vaporeon', 'Jolteon', 'Flareon', 'Porygon', 'Omanyte', 'Omastar', 'Kabuto', 'Kabutops', 'Aerodactyl', 'Snorlax', 'Articuno', 'Zapdos', 'Moltres', 'Dratini', 'Dragonair', 'Dragonite', 'Mewtwo', 'Mew'];
+const pokemonNames = ['Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise', 'Caterpie', 'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot', 'Rattata', 'Raticate', 'Spearow', 'Fearow', 'Ekans', 'Arbok', 'Pikachu', 'Raichu', 'Sandshrew', 'Sandslash', 'Nidoran♀', 'Nidorina', 'Nidoqueen', 'Nidoran♂‚', 'Nidorino', 'Nidoking', 'Clefairy', 'Clefable', 'Vulpix', 'Ninetales', 'Jigglypuff', 'Wigglytuff', 'Zubat', 'Golbat', 'Oddish', 'Gloom', 'Vileplume', 'Paras', 'Parasect', 'Venonat', 'Venomoth', 'Diglett', 'Dugtrio', 'Meowth', 'Persian', 'Psyduck', 'Golduck', 'Mankey', 'Primeape', 'Growlithe', 'Arcanine', 'Poliwag', 'Poliwhirl', 'Poliwrath', 'Abra', 'Kadabra', 'Alakazam', 'Machop', 'Machoke', 'Machamp', 'Bellsprout', 'Weepinbell', 'Victreebel', 'Tentacool', 'Tentacruel', 'Geodude', 'Graveler', 'Golem', 'Ponyta', 'Rapidash', 'Slowpoke', 'Slowbro', 'Magnemite', 'Magneton', "Farfetch'd", 'Doduo', 'Dodrio', 'Seel', 'Dewgong', 'Grimer', 'Muk', 'Shellder', 'Cloyster', 'Gastly', 'Haunter', 'Gengar', 'Onix', 'Drowzee', 'Hypno', 'Krabby', 'Kingler', 'Voltorb', 'Electrode', 'Exeggcute', 'Exeggutor', 'Cubone', 'Marowak', 'Hitmonlee', 'Hitmonchan', 'Lickitung', 'Koffing', 'Weezing', 'Rhyhorn', 'Rhydon', 'Chansey', 'Tangela', 'Kangaskhan', 'Horsea', 'Seadra', 'Goldeen', 'Seaking', 'Staryu', 'Starmie', 'Mr. Mime', 'Scyther', 'Jynx', 'Electabuzz', 'Magmar', 'Pinsir', 'Tauros', 'Magikarp', 'Gyarados', 'Lapras', 'Ditto', 'Eevee', 'Vaporeon', 'Jolteon', 'Flareon', 'Porygon', 'Omanyte', 'Omastar', 'Kabuto', 'Kabutops', 'Aerodactyl', 'Snorlax', 'Articuno', 'Zapdos', 'Moltres', 'Dratini', 'Dragonair', 'Dragonite', 'Mewtwo', 'Mew', 'Chikorita', 'Bayleef', 'Meganium', 'Cyndaquil', 'Quilava', 'Typhlosion', 'Totodile', 'Croconaw', 'Feraligatr', 'Sentret', 'Furret', 'Hoothoot', 'Noctowl', 'Ledyba', 'Ledian', 'Spinarak', 'Ariados', 'Crobat', 'Chinchou', 'Lanturn', 'Pichu', 'Cleffa', 'Igglybuff', 'Togepi', 'Togetic', 'Natu', 'Xatu', 'Mareep', 'Flaaffy', 'Ampharos', 'Bellossom', 'Marill', 'Azumarill', 'Sudowoodo', 'Politoed', 'Hoppip', 'Skiploom', 'Jumpluff', 'Aipom', 'Sunkern', 'Sunflora', 'Yanma', 'Wooper', 'Quagsire', 'Espeon', 'Umbreon', 'Murkrow', 'Slowking', 'Misdreavus', 'Unown', 'Wobbuffet', 'Girafarig', 'Pineco', 'Forretress', 'Dunsparce', 'Gligar', 'Steelix', 'Snubbull', 'Granbull', 'Qwilfish', 'Scizor', 'Shuckle', 'Heracross', 'Sneasel', 'Teddiursa', 'Ursaring', 'Slugma', 'Magcargo', 'Swinub', 'Piloswine', 'Corsola', 'Remoraid', 'Octillery', 'Delibird', 'Mantine', 'Skarmory', 'Houndour', 'Houndoom', 'Kingdra', 'Phanpy', 'Donphan', 'Porygon2', 'Stantler', 'Smeargle', 'Tyrogue', 'Hitmontop', 'Smoochum', 'Elekid', 'Magby', 'Miltank', 'Blissey', 'Raikou', 'Entei', 'Suicune', 'Larvitar', 'Pupitar', 'Tyranitar', 'Lugia', 'Ho-oh', 'Celebi'];
 var generatedTeams = 0;
 
-function getPokemonID(allowWildcard){
-	let maxID = 150;
-	if(allowWildcard == true){
-		maxID = 152;
+function getPokemonID(allowWildcard, gen2){
+	if(!gen2){
+		let maxID = 150;
+		if(allowWildcard == true){
+			maxID = 152;
+		}
+		return Math.floor(Math.random() * (maxID - 1) + 1);
 	}
-	return Math.floor(Math.random() * (maxID - 1) + 1);
+	else{
+		let maxID = 252;
+
+		return Math.floor(Math.random() * (maxID - 1) + 1);;
+	}
 }
 
 //spin the wheel of chance, generate your team and update the label of how many times it's been done
@@ -33,17 +40,31 @@ function generateTeam(){
 	let team = [];
 	let allowWildcard = $('#allow_wildcard').prop('checked');
 	let forceKarp = $('#force_karp').prop('checked');
+	let gen2Generation = $('#stadium_2').prop('checked');
+	
 	for(let i = 0; i < 6; i++){
 		if(i == 0){
-			team[i] = getPokemonID(allowWildcard);
+			let id = getPokemonID(allowWildcard, gen2Generation);
+			while(true){
+				if((id == 150 && !allowWildcard) || (id == 249 && !allowWildcard) ||( id == 250  && !allowWildcard)){
+					id = getPokemonID(allowWildcard, gen2Generation);
+				}
+				else{
+					team[i] = id;
+					break;
+				}
+			}
 		}
 		else{
-			let id = getPokemonID(allowWildcard);
+			let id = getPokemonID(allowWildcard, gen2Generation);
 			
 			while(true){
 				if(team.includes(id)){
 					//get a new ID until we find one not already in the team
-					id = getPokemonID(allowWildcard);
+					id = getPokemonID(allowWildcard, gen2Generation);
+				}
+				else if((id == 150 && !allowWildcard) || (id == 249 && !allowWildcard) ||( id == 250  && !allowWildcard)){
+					id = getPokemonID(allowWildcard, gen2Generation);
 				}
 				else{
 					team[i] = id;
@@ -56,7 +77,12 @@ function generateTeam(){
 		team = replaceWithKarp(team);
 	}
 	
-	outputTeam(team);
+	if(!gen2Generation){
+		outputStadium1Team(team);
+	}
+	else{
+		outputStadium2Team(team);
+	}
 }
 
 //replace a random team member with a magikarp as punishment
@@ -68,7 +94,7 @@ function replaceWithKarp(team){
 }
 
 //display the team with the corresponding name and image
-function outputTeam(team){
+function outputStadium1Team(team){
 	let teamElements =  [''];
 	
 	for(let i = 0; i < team.length; i++){
@@ -85,6 +111,7 @@ function outputTeam(team){
 			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + baseSpriteURL + 'mr-mime' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
 		}
 		else if(team[i] == 150 || team[i] == 151){
+			console.log("Replaced " + team[i] + " with wildcard");
 			teamElements[i] = 'Wildcard!<br/><img style="width:64px;height:64px" src="https://img.pokemondb.net/sprites/home/normal/unown-qm.png" alt="Wildcard!"/>';
 		}
 		else{
@@ -94,4 +121,41 @@ function outputTeam(team){
 		$('#party_' + (i + 1)).html(teamElements[i]);
 	}
 }
+
+function outputStadium2Team(team){
+	let teamElements =  [''];
+	
+	for(let i = 0; i < team.length; i++){
+		if(team[i] == 29){
+			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + baseSpriteURL + 'nidoran-f' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
+		}
+		else if(team[i] == 32){
+			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + baseSpriteURL + 'nidoran-m' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
+		}
+		else if(team[i] == 83){
+			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + baseSpriteURL + 'farfetchd' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
+		}
+		else if(team[i] == 122){
+			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + baseSpriteURL + 'mr-mime' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
+		}
+		else if(team[i] == 150 || team[i] == 249 || team[i] == 250){
+			console.log("Replaced " + team[i] + " with wildcard");
+			teamElements[i] = 'Wildcard!<br/><img style="width:64px;height:64px" src="https://img.pokemondb.net/sprites/home/normal/unown-qm.png" alt="Wildcard!"/>';
+		}
+		else if(team[i] == 251){
+			console.log('celery');
+		}
+		else{
+			try{
+				teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + baseSpriteURL + pokemonNames[team[i] - 1].toLowerCase() + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
+			}
+			catch{
+				console.log(team[i]);
+			}
+		}
+		
+		$('#party_' + (i + 1)).html(teamElements[i]);
+	}
+}
+
 
