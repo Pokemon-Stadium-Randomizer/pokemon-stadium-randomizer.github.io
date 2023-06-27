35b4455a96061b1868d69cb96873fb8ddc2addff
Flak <FlakTheMighty@users.noreply.github.com>
2023-06-27 17:27:30 -0500

    Added rolling for shiny
    A 1 in 4096 chance of getting a shiny has been added to team generation

--- js/logic.js
+++ js/logic.js
@@ -1,6 +1,15 @@
 const baseSpriteURL = 'https://img.pokemondb.net/sprites/ruby-sapphire/normal/'
+const baseShinySpriteURL = 'https://img.pokemondb.net/sprites/ruby-sapphire/shiny/'
 const pokemonNames = ['Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise', 'Caterpie', 'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot', 'Rattata', 'Raticate', 'Spearow', 'Fearow', 'Ekans', 'Arbok', 'Pikachu', 'Raichu', 'Sandshrew', 'Sandslash', 'Nidoran♀', 'Nidorina', 'Nidoqueen', 'Nidoran♂‚', 'Nidorino', 'Nidoking', 'Clefairy', 'Clefable', 'Vulpix', 'Ninetales', 'Jigglypuff', 'Wigglytuff', 'Zubat', 'Golbat', 'Oddish', 'Gloom', 'Vileplume', 'Paras', 'Parasect', 'Venonat', 'Venomoth', 'Diglett', 'Dugtrio', 'Meowth', 'Persian', 'Psyduck', 'Golduck', 'Mankey', 'Primeape', 'Growlithe', 'Arcanine', 'Poliwag', 'Poliwhirl', 'Poliwrath', 'Abra', 'Kadabra', 'Alakazam', 'Machop', 'Machoke', 'Machamp', 'Bellsprout', 'Weepinbell', 'Victreebel', 'Tentacool', 'Tentacruel', 'Geodude', 'Graveler', 'Golem', 'Ponyta', 'Rapidash', 'Slowpoke', 'Slowbro', 'Magnemite', 'Magneton', "Farfetch'd", 'Doduo', 'Dodrio', 'Seel', 'Dewgong', 'Grimer', 'Muk', 'Shellder', 'Cloyster', 'Gastly', 'Haunter', 'Gengar', 'Onix', 'Drowzee', 'Hypno', 'Krabby', 'Kingler', 'Voltorb', 'Electrode', 'Exeggcute', 'Exeggutor', 'Cubone', 'Marowak', 'Hitmonlee', 'Hitmonchan', 'Lickitung', 'Koffing', 'Weezing', 'Rhyhorn', 'Rhydon', 'Chansey', 'Tangela', 'Kangaskhan', 'Horsea', 'Seadra', 'Goldeen', 'Seaking', 'Staryu', 'Starmie', 'Mr. Mime', 'Scyther', 'Jynx', 'Electabuzz', 'Magmar', 'Pinsir', 'Tauros', 'Magikarp', 'Gyarados', 'Lapras', 'Ditto', 'Eevee', 'Vaporeon', 'Jolteon', 'Flareon', 'Porygon', 'Omanyte', 'Omastar', 'Kabuto', 'Kabutops', 'Aerodactyl', 'Snorlax', 'Articuno', 'Zapdos', 'Moltres', 'Dratini', 'Dragonair', 'Dragonite', 'Mewtwo', 'Mew', 'Chikorita', 'Bayleef', 'Meganium', 'Cyndaquil', 'Quilava', 'Typhlosion', 'Totodile', 'Croconaw', 'Feraligatr', 'Sentret', 'Furret', 'Hoothoot', 'Noctowl', 'Ledyba', 'Ledian', 'Spinarak', 'Ariados', 'Crobat', 'Chinchou', 'Lanturn', 'Pichu', 'Cleffa', 'Igglybuff', 'Togepi', 'Togetic', 'Natu', 'Xatu', 'Mareep', 'Flaaffy', 'Ampharos', 'Bellossom', 'Marill', 'Azumarill', 'Sudowoodo', 'Politoed', 'Hoppip', 'Skiploom', 'Jumpluff', 'Aipom', 'Sunkern', 'Sunflora', 'Yanma', 'Wooper', 'Quagsire', 'Espeon', 'Umbreon', 'Murkrow', 'Slowking', 'Misdreavus', 'Unown', 'Wobbuffet', 'Girafarig', 'Pineco', 'Forretress', 'Dunsparce', 'Gligar', 'Steelix', 'Snubbull', 'Granbull', 'Qwilfish', 'Scizor', 'Shuckle', 'Heracross', 'Sneasel', 'Teddiursa', 'Ursaring', 'Slugma', 'Magcargo', 'Swinub', 'Piloswine', 'Corsola', 'Remoraid', 'Octillery', 'Delibird', 'Mantine', 'Skarmory', 'Houndour', 'Houndoom', 'Kingdra', 'Phanpy', 'Donphan', 'Porygon2', 'Stantler', 'Smeargle', 'Tyrogue', 'Hitmontop', 'Smoochum', 'Elekid', 'Magby', 'Miltank', 'Blissey', 'Raikou', 'Entei', 'Suicune', 'Larvitar', 'Pupitar', 'Tyranitar', 'Lugia', 'Ho-oh', 'Celebi'];
 var generatedTeams = 0;
+var numberOfShinies = 0;
+
+function reload(){
+	generatedTeams = 0;
+	numberOfShinies = 0;
+	$('#team_info').html('You have generated ' + generatedTeams + ' team' + ((generatedTeams == 0 || generatedTeams > 1) ? 's.' : '.'));
+	$('#team_info').append('<br/>You have generated ' + numberOfShinies + ' Shiny Pokémon.');
+}
 
 function getPokemonID(allowWildcard, gen2){
 	if(!gen2){
@@ -33,6 +42,14 @@ function getNewTeam(){
 	
 	generatedTeams++;
 	$('#team_info').html('You have generated ' + generatedTeams + ' team' + ((generatedTeams == 0 || generatedTeams > 1) ? 's.' : '.'));
+	$('#team_info').append('<br/>You have generated ' + numberOfShinies + ' Shiny Pokémon.');
+}
+
+function getNewTeamUntilShiny(){
+	let currentShinyCount = numberOfShinies;
+	do{
+		getNewTeam();
+	}while(numberOfShinies == currentShinyCount);
 }
 
 //get an array of 6 ids to use to show the team
@@ -41,7 +58,6 @@ function generateTeam(){
 	let allowWildcard = $('#allow_wildcard').prop('checked');
 	let forceKarp = $('#force_karp').prop('checked');
 	let gen2Generation = $('#stadium_2').prop('checked');
-	
 	for(let i = 0; i < 6; i++){
 		if(i == 0){
 			let id = getPokemonID(allowWildcard, gen2Generation);
@@ -85,6 +101,23 @@ function generateTeam(){
 	}
 }
 
+function rollForShiny(){
+	//upper limit for chances of shiny
+	let MAX_NUMBER = 4096;
+	//number to hit for a shiny
+	let MIN_NUMBER = 1;
+	let isShiny = false;
+	
+	let roll = Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER) + MIN_NUMBER);
+	
+	if(roll == MIN_NUMBER){
+		isShiny = true;
+		numberOfShinies++;
+	}
+	
+	return isShiny;
+}
+
 //replace a random team member with a magikarp as punishment
 function replaceWithKarp(team){
 	let slot = Math.floor(Math.random() * (5 - 0) + 1);
@@ -98,24 +131,31 @@ function outputStadium1Team(team){
 	let teamElements =  [''];
 	
 	for(let i = 0; i < team.length; i++){
+		let usedURL = baseSpriteURL;
+		
+		//replace the URL with the shiny URL if we get a shiny :)
+		if(rollForShiny()){
+			usedURL = baseShinySpriteURL;
+		}
+		
 		if(team[i] == 29){
-			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + baseSpriteURL + 'nidoran-f' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
+			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + usedURL + 'nidoran-f' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
 		}
 		else if(team[i] == 32){
-			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + baseSpriteURL + 'nidoran-m' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
+			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + usedURL + 'nidoran-m' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
 		}
 		else if(team[i] == 83){
-			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + baseSpriteURL + 'farfetchd' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
+			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + usedURL + 'farfetchd' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
 		}
 		else if(team[i] == 122){
-			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + baseSpriteURL + 'mr-mime' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
+			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + usedURL + 'mr-mime' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
 		}
 		else if(team[i] == 150 || team[i] == 151){
 			console.log("Replaced " + team[i] + " with wildcard");
 			teamElements[i] = 'Wildcard!<br/><img style="width:64px;height:64px" src="https://img.pokemondb.net/sprites/home/normal/unown-qm.png" alt="Wildcard!"/>';
 		}
 		else{
-			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + baseSpriteURL + pokemonNames[team[i] - 1].toLowerCase() + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
+			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + usedURL + pokemonNames[team[i] - 1].toLowerCase() + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
 		}
 		
 		$('#party_' + (i + 1)).html(teamElements[i]);
@@ -126,24 +166,31 @@ function outputStadium2Team(team){
 	let teamElements =  [''];
 	
 	for(let i = 0; i < team.length; i++){
+		let usedURL = baseSpriteURL;
+		
+		//replace the URL with the shiny URL if we get a shiny :)
+		if(rollForShiny()){
+			usedURL = baseShinySpriteURL;	
+		}
+		
 		if(team[i] == 29){
-			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + baseSpriteURL + 'nidoran-f' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
+			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + usedURL + 'nidoran-f' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
 		}
 		else if(team[i] == 32){
-			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + baseSpriteURL + 'nidoran-m' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
+			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + usedURL + 'nidoran-m' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
 		}
 		else if(team[i] == 83){
-			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + baseSpriteURL + 'farfetchd' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
+			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + usedURL + 'farfetchd' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
 		}
 		else if(team[i] == 122){
-			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + baseSpriteURL + 'mr-mime' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
+			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + usedURL + 'mr-mime' + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
 		}
 		else if(team[i] == 150 || team[i] == 249 || team[i] == 250){
 			console.log("Replaced " + team[i] + " with wildcard");
 			teamElements[i] = 'Wildcard!<br/><img style="width:64px;height:64px" src="https://img.pokemondb.net/sprites/home/normal/unown-qm.png" alt="Wildcard!"/>';
 		}
 		else{
-			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + baseSpriteURL + pokemonNames[team[i] - 1].toLowerCase() + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
+			teamElements[i] = pokemonNames[team[i] - 1] + '<br/><img src="' + usedURL + pokemonNames[team[i] - 1].toLowerCase() + '.png" alt="' + pokemonNames[team[i] - 1] + '"/>'; 
 		}
 		
 		$('#party_' + (i + 1)).html(teamElements[i]);
